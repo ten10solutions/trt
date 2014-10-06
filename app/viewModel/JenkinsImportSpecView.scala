15f151bbf50048d58944db6868dbfa75866f3092
Matt Russell <MattRussellUK@gmail.com>
2014-10-06 14:34:14 +0100

    Jenkins import spec tweaks

--- app/viewModel/JenkinsImportSpecView.scala
+++ app/viewModel/JenkinsImportSpecView.scala
@@ -8,6 +8,15 @@ case class JenkinsImportSpecView(spec: JenkinsImportSpec, inProgress: Boolean) {
 
   def id = spec.id
 
+  def jobName: String = {
+    val path = jobUrl.getPath
+    val lastJobIndex = path.lastIndexOf("/job/")
+    if (lastJobIndex == -1)
+      jobUrl.toString
+    else
+      path.drop(lastJobIndex + 5)
+  }
+
   def jobUrl = spec.jobUrl
 
   def pollingInterval = DateUtils.describeDuration(spec.pollingInterval)
