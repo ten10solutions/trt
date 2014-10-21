348a31e545e6ec996cb6fe27ce6f020a5087fc91
Matt Russell <MattRussellUK@gmail.com>
2014-10-21 18:29:45 +0100

    tidy up import spec code; better display name for CI import specs

--- app/viewModel/CiImportSpecView.scala
+++ app/viewModel/CiImportSpecView.scala
@@ -4,12 +4,19 @@ import com.thetestpeople.trt.model.jenkins.CiImportSpec
 import com.thetestpeople.trt.model.jenkins._
 import com.thetestpeople.trt.utils.DateUtils
 import com.thetestpeople.trt.model.CiType
+import com.thetestpeople.trt.importer.teamcity.TeamCityUrlParser
 
 case class CiImportSpecView(spec: CiImportSpec, inProgress: Boolean) {
 
   def id = spec.id
 
-  def jobName: String = {
+  def jobName: String =
+    TeamCityUrlParser.parse(jobUrl) match {
+      case Left(_)  ⇒ jenkinsJobName
+      case Right(l) ⇒ l.buildTypeId
+    }
+
+  private def jenkinsJobName = {
     val path = jobUrl.getPath
     val lastJobIndex = path.lastIndexOf("/job/")
     if (lastJobIndex == -1)
